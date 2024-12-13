efe338cda8a1e6668e333d501f582ce9cf221127
oloshe <jacob.j@qq.com>
2024-12-13 23:33:56 +0800

    feat: 支持缓存

--- index.js
+++ index.js
@@ -12,38 +12,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createHttpClient = void 0;
+exports.createHttpClient = exports.jsonBigInt = void 0;
 const json_bigint_1 = __importDefault(require("json-bigint"));
+exports.jsonBigInt = json_bigint_1.default;
 const createHttpClient = (options) => {
-    var _a, _b, _c;
+    var _a, _b, _c, _d, _e, _f, _g, _h;
     const client = {
-        prefix: options.prefix,
-        alias: (_a = options.alias) !== null && _a !== void 0 ? _a : '',
-        headers: Object.assign({}, options.headers),
+        prefix: (_a = options === null || options === void 0 ? void 0 : options.prefix) !== null && _a !== void 0 ? _a : "",
+        alias: (_b = options === null || options === void 0 ? void 0 : options.alias) !== null && _b !== void 0 ? _b : "",
+        headers: Object.assign({}, options === null || options === void 0 ? void 0 : options.headers),
         hooks: {
-            beforeRequest: options.hooks.beforeRequest || [],
-            afterResponse: options.hooks.afterResponse || [],
-            beforeRetry: options.hooks.beforeRetry || [],
-            catchError: options.hooks.catchError || []
+            beforeRequest: ((_c = options === null || options === void 0 ? void 0 : options.hooks) === null || _c === void 0 ? void 0 : _c.beforeRequest) || [],
+            afterResponse: ((_d = options === null || options === void 0 ? void 0 : options.hooks) === null || _d === void 0 ? void 0 : _d.afterResponse) || [],
+            beforeRetry: ((_e = options === null || options === void 0 ? void 0 : options.hooks) === null || _e === void 0 ? void 0 : _e.beforeRetry) || [],
+            catchError: ((_f = options === null || options === void 0 ? void 0 : options.hooks) === null || _f === void 0 ? void 0 : _f.catchError) || [],
         },
-        retryCount: (_b = options.retryCount) !== null && _b !== void 0 ? _b : 0,
-        retryTimeout: (_c = options.retryTimeout) !== null && _c !== void 0 ? _c : 0,
-        get: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: 'GET' })),
-        post: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: 'POST' })),
-        put: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: 'PUT' })),
-        delete: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: 'DELETE' })),
-        patch: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: 'PATCH' })),
-        head: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: 'HEAD' }))
+        retryCount: (_g = options === null || options === void 0 ? void 0 : options.retryCount) !== null && _g !== void 0 ? _g : 0,
+        retryTimeout: (_h = options === null || options === void 0 ? void 0 : options.retryTimeout) !== null && _h !== void 0 ? _h : 0,
+        get: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: "GET" })),
+        post: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: "POST" })),
+        put: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: "PUT" })),
+        delete: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: "DELETE" })),
+        patch: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: "PATCH" })),
+        head: (url, options) => sendSequest(client, Object.assign(Object.assign({}, options), { url, method: "HEAD" })),
     };
     return client;
 };
 exports.createHttpClient = createHttpClient;
 const sendSequest = (client, options) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b;
+    var _a, _b, _c, _d, _e;
     const req = Object.assign(options, {
         headers: Object.assign(Object.assign({}, client.headers), options.headers),
         searchParams: options.searchParams || {},
-        data: options.data || {}
+        data: options.data || {},
     });
     // 重试次数
     const retryCount = (_a = options.retryCount) !== null && _a !== void 0 ? _a : client.retryCount;
@@ -55,7 +56,7 @@ const sendSequest = (client, options) => __awaiter(void 0, void 0, void 0, funct
     const query = new URLSearchParams(req.searchParams).toString();
     const url = query ? `${baseUrl}?${query}` : baseUrl;
     let body;
-    if (req.method !== 'GET' && req.method !== 'HEAD') {
+    if (req.method !== "GET" && req.method !== "HEAD") {
         if (req.data instanceof FormData) {
             body = req.data;
         }
@@ -64,28 +65,56 @@ const sendSequest = (client, options) => __awaiter(void 0, void 0, void 0, funct
         }
     }
     let json;
-    try {
-        const response = yield fetch(url, {
-            method: req.method,
-            body: body,
-            headers: req.headers
-        });
-        json = yield response.json();
+    // 缓存时间
+    const cacheTime = (_d = (_c = options.cache) === null || _c === void 0 ? void 0 : _c.milliseconds) !== null && _d !== void 0 ? _d : 0;
+    // 是否使用缓存
+    const useCache = cacheTime > 0;
+    let cacheKey;
+    // 若使用缓存，则先尝试从缓存中获取数据
+    if (useCache) {
+        if (((_e = options.cache) === null || _e === void 0 ? void 0 : _e.matchType) === "path") {
+            cacheKey = url.split("?")[0];
+        }
+        else {
+            cacheKey = url;
+        }
+        const cacheData = PRETTY_CACHE.get(cacheKey);
+        if (cacheData) {
+            // 缓存未过期
+            const response = cacheData;
+            json = json_bigint_1.default.parse(yield response.text());
+        }
     }
-    catch (e) {
-        // 重试
-        if (retryCount > 0) {
-            console.log('retry');
-            for (const fn of client.hooks.beforeRetry) {
-                fn(client, options);
+    // 如果没有命中缓存，则发送请求
+    if (!json) {
+        try {
+            const response = yield fetch(url, {
+                method: req.method,
+                body: body,
+                headers: req.headers,
+            });
+            if (useCache) {
+                PRETTY_CACHE.set(cacheKey, response, cacheTime);
             }
-            yield delay(client.retryTimeout);
-            return sendSequest(client, Object.assign(Object.assign({}, options), { retryCount: retryCount - 1 }));
+            json = json_bigint_1.default.parse(yield response.text());
         }
-        for (const fn of client.hooks.catchError) {
-            fn(e);
+        catch (e) {
+            // 重试
+            if (retryCount > 0) {
+                // 触发hook
+                for (const fn of client.hooks.beforeRetry) {
+                    fn(client, options);
+                }
+                // 等待一段时间再重试
+                yield delay(client.retryTimeout);
+                // 重新发送请求
+                return sendSequest(client, Object.assign(Object.assign({}, options), { retryCount: retryCount - 1 }));
+            }
+            for (const fn of client.hooks.catchError) {
+                fn(e);
+            }
+            throw e;
         }
-        throw e;
     }
     // 钩子: afterResponse
     // 如果返回值不为 undefined，则替换为返回值
@@ -97,4 +126,66 @@ const sendSequest = (client, options) => __awaiter(void 0, void 0, void 0, funct
     }
     return json;
 });
-const delay = (ms = 0) => new Promise(resolve => setTimeout(() => resolve(), ms));
+const delay = (ms = 0) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+class LRUCache {
+    constructor(maxSize) {
+        this.cache = new Map();
+        this.maxSize = maxSize;
+    }
+    /**
+     * 添加缓存
+     * @param key 键
+     * @param value 值
+     * @param activeTime 有效时间（毫秒）
+     */
+    set(key, value, activeTime) {
+        // 如果键已经存在，先删除它以移动到队尾
+        if (this.cache.has(key)) {
+            this.cache.delete(key);
+        }
+        const expirationTime = Date.now() + activeTime;
+        this.cache.set(key, { value, expirationTime });
+        // 如果超出最大容量，移除最旧的键
+        if (this.size > this.maxSize) {
+            const oldestKey = this.cache.keys().next().value;
+            oldestKey && this.cache.delete(oldestKey);
+        }
+    }
+    /**
+     * 获取缓存
+     * @param key 键
+     * @returns 值或者 null（过期或不存在时）
+     */
+    get(key) {
+        const item = this.cache.get(key);
+        if (!item)
+            return null;
+        const { value, expirationTime } = item;
+        // 如果已过期，删除缓存并返回 null
+        if (Date.now() > expirationTime) {
+            this.cache.delete(key);
+            return null;
+        }
+        // 移到队尾
+        this.cache.delete(key);
+        this.cache.set(key, item);
+        return value;
+    }
+    has(key) {
+        return this.cache.has(key);
+    }
+    /**
+     * 获取当前缓存大小
+     * @returns 缓存项的数量
+     */
+    get size() {
+        return this.cache.size;
+    }
+    /**
+     * 清空缓存
+     */
+    clear() {
+        this.cache.clear();
+    }
+}
+const PRETTY_CACHE = new LRUCache(10000);
--- index.ts
+++ index.ts
@@ -1,72 +1,92 @@
-import JSON_BIGINT from 'json-bigint'
+import JSON_BIGINT from "json-bigint";
+export const jsonBigInt = JSON_BIGINT;
 
 /**
  * 请求数据
  */
 export interface RequestData {
   /** 请求url */
-  url: string
+  url: string;
   /** Http方法 */
-  method: HttpMethod
+  method: HttpMethod;
   /** 请求头 */
-  headers: Record<string, string>
+  headers: Record<string, string>;
   /** url query 参数 */
-  searchParams: Record<string, string>
+  searchParams: Record<string, any>;
   /** body json 数据 */
-  data: Record<string, any> | FormData | null
+  data: Record<string, any> | FormData | null;
 }
 
-type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD'
+type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD";
 
 /**
  * 请求参数
  */
 export interface RequestOptions {
   /** 相对url */
-  url: string
+  url: string;
   /** http 方法 */
-  method: HttpMethod
+  method: HttpMethod;
   /** 请求头 */
-  headers?: Record<string, string>
+  headers?: Record<string, string>;
   /** 请求URL参数 */
-  searchParams?: Record<string, string>
+  searchParams?: Record<string, any>;
   /** 请求体 */
-  data?: Record<string, any> | FormData | null
+  data?: Record<string, any> | FormData | null;
   /** 重试次数 */
-  retryCount?: number
+  retryCount?: number;
+  /** 缓存规则，只有 GET 方法生效 */
+  cache?: CacheOptions;
 }
 
-export type BeforeRequest = (client: HttpClient, req: RequestData) => void | Promise<void>
-export type AfterResponse<T = any> = (client: HttpClient, req: RequestData, res: T) => void | T
-export type BeforeRetry = (client: HttpClient, options: RequestOptions) => void
-export type CatchError = (e: any) => void
+export interface CacheOptions {
+  /** 缓存的时长，单位毫秒 */
+  milliseconds?: number;
+  /** 缓存的匹配类型， 如果是 url 则是匹配完整的 url, 如果是 path 则仅匹配路径，默认为 url*/
+  matchType?: "url" | "path";
+}
+
+export type BeforeRequest = (
+  client: HttpClient,
+  req: RequestData
+) => void | Promise<void>;
+export type AfterResponse<T = any> = (
+  client: HttpClient,
+  req: RequestData,
+  res: T
+) => void | T;
+export type BeforeRetry = (client: HttpClient, options: RequestOptions) => void;
+export type CatchError = (e: any) => void;
 
 interface CreateHttpClientOptions<T> {
   /** 请求前缀 */
-  prefix: string
+  prefix?: string;
   /** 别名 */
-  alias?: string
+  alias?: string;
   /** 通用headers */
-  headers: Record<string, string>
+  headers?: Record<string, string>;
   /** 钩子 */
-  hooks: Partial<ClientHook<T>>
+  hooks?: Partial<ClientHook<T>>;
   /** 重试次数 */
-  retryCount?: number
+  retryCount?: number;
   /** 重试延迟 */
-  retryTimeout?: number
+  retryTimeout?: number;
 }
 
-type MethodRequest = <T>(url: string, options?: Partial<RequestOptions>) => Promise<T>
+type MethodRequest = <T>(
+  url: string,
+  options?: Partial<RequestOptions>
+) => Promise<T>;
 
 interface ClientHook<T> {
   /** 请求前钩子 */
-  beforeRequest: BeforeRequest[]
+  beforeRequest: BeforeRequest[];
   /** 请求后钩子 */
-  afterResponse: AfterResponse<T>[]
+  afterResponse: AfterResponse<T>[];
   /** 重试钩子 */
-  beforeRetry: BeforeRetry[]
+  beforeRetry: BeforeRetry[];
   /** 捕获错误钩子 */
-  catchError: CatchError[]
+  catchError: CatchError[];
 }
 
 /**
@@ -74,116 +94,235 @@ interface ClientHook<T> {
  */
 interface HttpClient<T = any> {
   /** 前缀 */
-  prefix: string
+  prefix: string;
   /** 别名 */
-  alias: string
+  alias: string;
   /** 通用请求头 */
-  headers: Record<string, string>
+  headers: Record<string, string>;
   /** 请求钩子 */
-  hooks: ClientHook<T>
-  retryCount: number
-  retryTimeout: number
+  hooks: ClientHook<T>;
+  /** 重试次数 */
+  retryCount: number;
+  /** 重试延迟 */
+  retryTimeout: number;
   /** GET 请求 */
-  get: MethodRequest
+  get: MethodRequest;
   /** POST 请求 */
-  post: MethodRequest
+  post: MethodRequest;
   /** PUT 请求 */
-  put: MethodRequest
+  put: MethodRequest;
   /** DELETE 请求 */
-  delete: MethodRequest
+  delete: MethodRequest;
   /** PATCH 请求 */
-  patch: MethodRequest
+  patch: MethodRequest;
   /** HEAD 请求 */
-  head: MethodRequest
+  head: MethodRequest;
 }
 
-export const createHttpClient = <T>(options: CreateHttpClientOptions<T>) => {
+export const createHttpClient = <T>(options?: CreateHttpClientOptions<T>) => {
   const client: HttpClient<T> = {
-    prefix: options.prefix,
-    alias: options.alias ?? '',
-    headers: { ...options.headers },
+    prefix: options?.prefix ?? "",
+    alias: options?.alias ?? "",
+    headers: { ...options?.headers },
     hooks: {
-      beforeRequest: options.hooks.beforeRequest || [],
-      afterResponse: options.hooks.afterResponse || [],
-      beforeRetry: options.hooks.beforeRetry || [],
-      catchError: options.hooks.catchError || []
+      beforeRequest: options?.hooks?.beforeRequest || [],
+      afterResponse: options?.hooks?.afterResponse || [],
+      beforeRetry: options?.hooks?.beforeRetry || [],
+      catchError: options?.hooks?.catchError || [],
     },
-    retryCount: options.retryCount ?? 0,
-    retryTimeout: options.retryTimeout ?? 0,
-    get: (url, options) => sendSequest(client, { ...options, url, method: 'GET' }),
-    post: (url, options) => sendSequest(client, { ...options, url, method: 'POST' }),
-    put: (url, options) => sendSequest(client, { ...options, url, method: 'PUT' }),
-    delete: (url, options) => sendSequest(client, { ...options, url, method: 'DELETE' }),
-    patch: (url, options) => sendSequest(client, { ...options, url, method: 'PATCH' }),
-    head: (url, options) => sendSequest(client, { ...options, url, method: 'HEAD' })
-  }
-  return client
-}
+    retryCount: options?.retryCount ?? 0,
+    retryTimeout: options?.retryTimeout ?? 0,
+    get: (url, options) =>
+      sendSequest(client, { ...options, url, method: "GET" }),
+    post: (url, options) =>
+      sendSequest(client, { ...options, url, method: "POST" }),
+    put: (url, options) =>
+      sendSequest(client, { ...options, url, method: "PUT" }),
+    delete: (url, options) =>
+      sendSequest(client, { ...options, url, method: "DELETE" }),
+    patch: (url, options) =>
+      sendSequest(client, { ...options, url, method: "PATCH" }),
+    head: (url, options) =>
+      sendSequest(client, { ...options, url, method: "HEAD" }),
+  };
+  return client;
+};
 
-const sendSequest = async <T>(client: HttpClient, options: RequestOptions): Promise<T> => {
+const sendSequest = async <T>(
+  client: HttpClient,
+  options: RequestOptions
+): Promise<T> => {
   const req: RequestData = Object.assign(options, {
     headers: { ...client.headers, ...options.headers },
     searchParams: options.searchParams || {},
-    data: options.data || {}
-  })
+    data: options.data || {},
+  });
 
   // 重试次数
-  const retryCount = options.retryCount ?? client.retryCount
+  const retryCount = options.retryCount ?? client.retryCount;
 
   // 钩子: beforeRequest
   for (const fn of client.hooks.beforeRequest ?? []) {
-    await fn(client, req)
+    await fn(client, req);
   }
 
-  const baseUrl = client.prefix + req.url
-  const query = new URLSearchParams(req.searchParams).toString()
-  const url = query ? `${baseUrl}?${query}` : baseUrl
+  const baseUrl = client.prefix + req.url;
+  const query = new URLSearchParams(req.searchParams).toString();
+  const url = query ? `${baseUrl}?${query}` : baseUrl;
 
-  let body: BodyInit | undefined
-  if (req.method !== 'GET' && req.method !== 'HEAD') {
+  let body: BodyInit | undefined;
+  if (req.method !== "GET" && req.method !== "HEAD") {
     if (req.data instanceof FormData) {
-      body = req.data
+      body = req.data;
     } else {
-      body = JSON_BIGINT.stringify(req.data)
+      body = JSON_BIGINT.stringify(req.data);
     }
   }
 
-  let json: T
-  try {
-    const response: Response = await fetch(url, {
-      method: req.method,
-      body: body,
-      headers: req.headers
-    })
+  let json!: T;
 
-    json = await response.json()
-  } catch (e: any) {
-    // 重试
-    if (retryCount > 0) {
-      console.log('retry')
-      for (const fn of client.hooks.beforeRetry) {
-        fn(client, options)
+  // 缓存时间
+  const cacheTime = options.cache?.milliseconds ?? 0;
+  // 是否使用缓存
+  const useCache = cacheTime > 0;
+  let cacheKey: string | undefined;
+  // 若使用缓存，则先尝试从缓存中获取数据
+  if (useCache) {
+    if (options.cache?.matchType === "path") {
+      cacheKey = url.split("?")[0];
+    } else {
+      cacheKey = url;
+    }
+    const cacheData = PRETTY_CACHE.get(cacheKey);
+    if (cacheData) {
+      // 缓存未过期
+      const response = cacheData;
+      json = JSON_BIGINT.parse(await response.text());
+    }
+  }
+
+  // 如果没有命中缓存，则发送请求
+  if (!json) {
+    try {
+      const response: Response = await fetch(url, {
+        method: req.method,
+        body: body,
+        headers: req.headers,
+      });
+
+      if (useCache) {
+        PRETTY_CACHE.set(cacheKey!, response, cacheTime)
       }
-
-      await delay(client.retryTimeout)
-      return sendSequest(client, { ...options, retryCount: retryCount - 1 })
+  
+      json = JSON_BIGINT.parse(await response.text());
+    } catch (e: any) {
+      // 重试
+      if (retryCount > 0) {
+        // 触发hook
+        for (const fn of client.hooks.beforeRetry) {
+          fn(client, options);
+        }
+  
+        // 等待一段时间再重试
+        await delay(client.retryTimeout);
+        // 重新发送请求
+        return sendSequest(client, { ...options, retryCount: retryCount - 1 });
+      }
+      for (const fn of client.hooks.catchError) {
+        fn(e);
+      }
+      throw e;
     }
-    for (const fn of client.hooks.catchError) {
-      fn(e)
-    }
-    throw e
   }
 
   // 钩子: afterResponse
   // 如果返回值不为 undefined，则替换为返回值
   for (const fn of client.hooks.afterResponse) {
-    const result = fn(client, req, json)
+    const result = fn(client, req, json);
     if (result !== undefined) {
-      json = result
+      json = result;
     }
   }
 
-  return json
+  return json;
+};
+
+const delay = (ms: number = 0) =>
+  new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
+
+class LRUCache<K, V> {
+  private cache: Map<K, { value: V; expirationTime: number }>;
+  private maxSize: number;
+
+  constructor(maxSize: number) {
+      this.cache = new Map<K, { value: V; expirationTime: number }>();
+      this.maxSize = maxSize;
+  }
+
+  /**
+   * 添加缓存
+   * @param key 键
+   * @param value 值
+   * @param activeTime 有效时间（毫秒）
+   */
+  set(key: K, value: V, activeTime: number): void {
+      // 如果键已经存在，先删除它以移动到队尾
+      if (this.cache.has(key)) {
+          this.cache.delete(key);
+      }
+
+      const expirationTime = Date.now() + activeTime;
+      this.cache.set(key, { value, expirationTime });
+
+      // 如果超出最大容量，移除最旧的键
+      if (this.size > this.maxSize) {
+          const oldestKey = this.cache.keys().next().value;
+          oldestKey && this.cache.delete(oldestKey);
+      }
+  }
+
+  /**
+   * 获取缓存
+   * @param key 键
+   * @returns 值或者 null（过期或不存在时）
+   */
+  get(key: K): V | null {
+      const item = this.cache.get(key);
+      if (!item) return null;
+
+      const { value, expirationTime } = item;
+
+      // 如果已过期，删除缓存并返回 null
+      if (Date.now() > expirationTime) {
+          this.cache.delete(key);
+          return null;
+      }
+
+      // 移到队尾
+      this.cache.delete(key);
+      this.cache.set(key, item);
+
+      return value;
+  }
+
+  has(key: K): boolean {
+      return this.cache.has(key);
+  }
+
+  /**
+   * 获取当前缓存大小
+   * @returns 缓存项的数量
+   */
+  get size(): number {
+      return this.cache.size;
+  }
+
+  /**
+   * 清空缓存
+   */
+  clear(): void {
+      this.cache.clear();
+  }
 }
 
-const delay = (ms: number = 0) => new Promise<void>(resolve => setTimeout(() => resolve(), ms))
+const PRETTY_CACHE = new LRUCache<string, Response>(10000);
